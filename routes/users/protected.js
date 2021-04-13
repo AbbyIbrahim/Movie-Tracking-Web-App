562982953df0dc8c9458d6c780e457a176539e4a
Abby <60075217+AbbyIbrahim@users.noreply.github.com>
2021-04-13 18:29:34 -0400

    Add files via upload

--- routes/users/protected.js
+++ routes/users/protected.js
@@ -1,11 +1,38 @@
 var express = require("express");
-const isUser = require("./isUser");
+const Movie = require("../../lib/mongoose/movie");
+const User = require("../../lib/mongoose/user");
+const isUser = require("../middlewares/isUser");
 var router = express.Router();
 
 router.use(isUser);
 
 router.get("/dashboard", async (req, res, next) => {
 	res.render("users/dashboard", { title: "Dashboard" });
+});
+
+router.get("/notifications", async (req, res, next) => {
+	const user = req.user;
+	const notifications = [];
+
+	// load notifications
+	for await (let noti of user.notifications) {
+		if (noti.type === "review") {
+			const movie = await Movie.findById(noti.movie);
+			const user = await User.findById(noti.user);
+			notifications.push(
+				`<a href="/users/${user._id}/profile">${user.name}</a> reviewed <a href="/movies/${movie._id}/">${movie.title}</a>`
+			);
+		}
+	}
+
+	// delete notifications
+	user.notifications = [];
+	await user.save();
+
+	res.render("users/notifications", {
+		title: "Notifications",
+		notifications,
+	});
 });
 
 router.get("/profile", async (req, res) => {
@@ -48,7 +75,7 @@ router.get("/:id/follow-toggle", async (req, res, next) => {
 
 	try {
 		await user.save();
-		res.redirect("/users/" + id);
+		res.redirect("/users/" + id + "/profile");
 	} catch (error) {
 		next(error);
 	}
